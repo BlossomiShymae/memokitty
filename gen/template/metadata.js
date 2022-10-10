2ea4f131744e115dc432810ff7f000c34bedb73f
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-10-10 01:15:52 -0500

    Update metadata

--- gen/template/metadata.js
+++ gen/template/metadata.js
@@ -3,6 +3,7 @@
 // @version      0.1.0
 // @description  Have templates for issues, pull requests, and more ready to go!
 // @author       MissUwuieTime
+// @updateURL    https://github.com/MissUwuieTime/memokitty/raw/main/dist/memokitty.user.js
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        GM_setValue
